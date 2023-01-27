f2b6ea4a1f4f9ef23fa874be46a6f4a1f71da8ff
Verum <asterfm2000@gmail.com>
2023-01-27 11:17:24 +0300

    1

---
+++ TestGit1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TestGit1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, resAdd;
+            Console.WriteLine("Input first number: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input second number: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            resAdd = a + b;
+            Console.WriteLine("Result: " + resAdd);
+        }
+    }
+}
